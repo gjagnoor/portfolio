15b2e91f0c8abfbfd511ada55552ff0f46de9fa1
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-19 07:04:10 +0530

    removed chalk webpack

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `noor grewal`,
+    description: `portfolio`,
+    author: `@noorgrewal`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `noor-grewal`,
+        short_name: `noorgrewal`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/nglogo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
